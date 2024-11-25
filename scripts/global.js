dd16fb58d2e03eb56e400e919380a1f8008f65ba
Priultimus <29077634+Priultimus@users.noreply.github.com>
2024-11-24 19:21:36 -0500

    add order modal

--- scripts/global.js
+++ scripts/global.js
@@ -1,17 +1,45 @@
-let modal = document.querySelector(".modal");
-let signInButton = document.querySelector(".sign-in");
-let submitButton = document.querySelector(".submit");
+let userModal = document.querySelector(".user-modal");
+let showModalButton = document.querySelector(".sign-in-link");
+let closeModalButton = document.querySelector(".modal-close");
+let submitButton = document.querySelector(".modal-submit");
 
-signInButton.onclick = function () {
-    modal.style.display = "block";
+let signInForm = document.querySelector(".sign-in.modal-form");
+let signUpForm = document.querySelector(".sign-up.modal-form");
+
+let signInLink = document.querySelector(".account-prompt.sign-in");
+let signUpLink = document.querySelector(".account-prompt.sign-up");
+
+function showModal(modal) {
+    console.log("modal arriving");
+    modal.classList.add("visible");
 };
 
-submitButton.onclick = function () {
-    modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function hideModal() {
+    modal = document.querySelector(".modal.visible");
+    console.log("modal departing")
+    modal.classList.remove("visible");
 }
+
+function showSignUpForm() {
+    console.log("signInForm departing")
+    signInForm.classList.remove("visible");
+    console.log("signUpForm arriving")
+    signUpForm.classList.add("visible");
+}
+
+function showSignInForm() {
+    console.log("signUpForm departing")
+    signUpForm.classList.remove("visible");
+    console.log("signInForm arriving");
+    signInForm.classList.add("visible");
+}
+
+window.addEventListener("keydown", (e) => {
+    e.key === "Escape" ? hideModal() : null
+});
+
+showModalButton.addEventListener("click", () => showModal(userModal));
+closeModalButton.addEventListener("click", hideModal);
+submitButton.addEventListener("click", hideModal);
+signInLink.addEventListener("click", showSignInForm);
+signUpLink.addEventListener("click", showSignUpForm);
